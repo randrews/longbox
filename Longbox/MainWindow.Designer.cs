ed37e770c7aaf34824da551007da76a2e743688b
Ross Andrews <randrews@geekfu.org>
2014-12-26 23:58:37 -0600

    Made icons bigger

--- Longbox/MainWindow.Designer.cs
+++ Longbox/MainWindow.Designer.cs
@@ -40,13 +40,14 @@
             // 
             // menuBar
             // 
+            this.menuBar.ImageScalingSize = new System.Drawing.Size(32, 32);
             this.menuBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.openButton,
             this.fullScreenButton,
             this.pageNumberLabel});
             this.menuBar.Location = new System.Drawing.Point(0, 0);
             this.menuBar.Name = "menuBar";
-            this.menuBar.Size = new System.Drawing.Size(284, 25);
+            this.menuBar.Size = new System.Drawing.Size(284, 39);
             this.menuBar.TabIndex = 0;
             this.menuBar.Text = "toolStrip1";
             // 
@@ -56,7 +57,7 @@
             this.openButton.Image = global::Longbox.Properties.Resources.Open_6529_24;
             this.openButton.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.openButton.Name = "openButton";
-            this.openButton.Size = new System.Drawing.Size(23, 22);
+            this.openButton.Size = new System.Drawing.Size(36, 36);
             this.openButton.Text = "Open file...";
             this.openButton.Click += new System.EventHandler(this.openButton_Click);
             // 
@@ -66,14 +67,14 @@
             this.fullScreenButton.Image = global::Longbox.Properties.Resources.FullScreen_32;
             this.fullScreenButton.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.fullScreenButton.Name = "fullScreenButton";
-            this.fullScreenButton.Size = new System.Drawing.Size(23, 22);
+            this.fullScreenButton.Size = new System.Drawing.Size(36, 36);
             this.fullScreenButton.Text = "Full screen";
             this.fullScreenButton.Click += new System.EventHandler(this.fullScreenButton_Click);
             // 
             // pageNumberLabel
             // 
             this.pageNumberLabel.Name = "pageNumberLabel";
-            this.pageNumberLabel.Size = new System.Drawing.Size(0, 22);
+            this.pageNumberLabel.Size = new System.Drawing.Size(0, 36);
             this.pageNumberLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // openFileDialog
@@ -85,9 +86,9 @@
             // 
             this.pageView.BackColor = System.Drawing.Color.Black;
             this.pageView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pageView.Location = new System.Drawing.Point(0, 25);
+            this.pageView.Location = new System.Drawing.Point(0, 39);
             this.pageView.Name = "pageView";
-            this.pageView.Size = new System.Drawing.Size(284, 236);
+            this.pageView.Size = new System.Drawing.Size(284, 222);
             this.pageView.TabIndex = 1;
             this.pageView.Window = null;
             // 
@@ -100,6 +101,7 @@
             this.Controls.Add(this.menuBar);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
+            this.MaximizeBox = false;
             this.Name = "MainWindow";
             this.Text = "Longbox";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainWindow_KeyDown);
